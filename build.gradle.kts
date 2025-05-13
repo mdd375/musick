2fadf086357295a76b2a6e78b65f158185d49144
Mikhail Dunaev <mdd375@yandex.ru>
2025-05-13 19:01:49 +0400

    Добавь базовые настройки swagger и spring security

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,10 +19,12 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-//	implementation("org.springframework.boot:spring-boot-starter-security")
+  	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.flywaydb:flyway-database-postgresql")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
---
+++ src/main/java/ru/m0vt/musick/config/OpenApiConfig.java
@@ -0,0 +1,53 @@
+package ru.m0vt.musick.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI musickOpenAPI() {
+        return new OpenAPI()
+            .info(
+                new Info()
+                    .title("Musick API")
+                    .description("API для музыкального сервиса Musick")
+                    .version("v1.0")
+                    .contact(
+                        new Contact()
+                            .name("Mikhail")
+                            .url("https://github.com/mdd375")
+                            .email("m0vt@example.com")
+                    )
+            )
+            .servers(
+                List.of(
+                    new Server()
+                        .url("http://localhost:8080")
+                        .description("Local server"),
+                    new Server()
+                        .url("https://api.musick.m0vt.ru")
+                        .description("Production server")
+                )
+            )
+            .components(
+                new Components()
+                    .addSecuritySchemes(
+                        "bearer-key",
+                        new SecurityScheme()
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme("bearer")
+                            .bearerFormat("JWT")
+                    )
+            );
+    }
+}
